9e7dc2d5b3de3a3deb03bc1ef4058adf2a043459
Tobi Ebenezer <tobisamuel70@gmail.com>
2025-06-23 16:37:24 +0200

    added Push notifications

--- script/run_recurrent_attn.sh
+++ script/run_recurrent_attn.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Set A - Default
-python main.py recurrent_attn.meta_lr=2.5e-5 recurrent_attn.inner_lr=1e-4 recurrent_attn.lstm_hidden_dim=128 recurrent_attn.mlp_hidden_dim=128 recurrent_attn.num_attention_heads=4 recurrent_attn.meta_batch_size=16
+python main.py attention_recurrent_ml.meta_lr=2.5e-5 attention_recurrent_ml.inner_lr=1e-4 attention_recurrent_ml.lstm_hidden_dim=128 attention_recurrent_ml.mlp_hidden_dim=128 attention_recurrent_ml.num_attention_heads=4 attention_recurrent_ml.meta_batch_size=16
 
 # Set B - More attention
-python main.py recurrent_attn.meta_lr=2e-5 recurrent_attn.inner_lr=5e-5 recurrent_attn.lstm_hidden_dim=128 recurrent_attn.mlp_hidden_dim=256 recurrent_attn.num_attention_heads=8 recurrent_attn.meta_batch_size=16
+python main.py attention_recurrent_ml.meta_lr=2e-5 attention_recurrent_ml.inner_lr=5e-5 attention_recurrent_ml.lstm_hidden_dim=128 attention_recurrent_ml.mlp_hidden_dim=256 attention_recurrent_ml.num_attention_heads=8 attention_recurrent_ml.meta_batch_size=16
 
 # Set C - Smaller attention
-python main.py recurrent_attn.meta_lr=5e-5 recurrent_attn.inner_lr=2e-4 recurrent_attn.lstm_hidden_dim=64 recurrent_attn.mlp_hidden_dim=64 recurrent_attn.num_attention_heads=2 recurrent_attn.meta_batch_size=8
+python main.py attention_recurrent_ml.meta_lr=5e-5 attention_recurrent_ml.inner_lr=2e-4 attention_recurrent_ml.lstm_hidden_dim=64 attention_recurrent_ml.mlp_hidden_dim=64 attention_recurrent_ml.num_attention_heads=2 attention_recurrent_ml.meta_batch_size=8
